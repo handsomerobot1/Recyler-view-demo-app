939c8f214ff83e7b27da8755d595a52f548133b0
HandsomeRobot <alaminjoy456@gmail.com>
2024-10-17 12:04:55 +0600

    model done

---
+++ app/src/main/java/com/example/recylerview/adapter/PlayerAdapter.kt
@@ -0,0 +1,11 @@
+package com.example.recylerview.adapter
+
+import androidx.recyclerview.widget.RecyclerView
+import com.example.recylerview.adapter.PlayerAdapter.*
+import com.example.recylerview.model.Player
+
+class PlayerAdapter(private val playerList:ArrayList<Player>):RecyclerView.Adapter<PlayerViewHolder>() {
+    class PlayerViewHolder (){
+
+    }
+}
---
+++ app/src/main/java/com/example/recylerview/model/Player.kt
@@ -0,0 +1,7 @@
+package com.example.recylerview.model
+
+data class Player(
+    var playername: String,
+    var playerDescription: String,
+    var playerimage: Int
+)
